5b15c6d0f146ce50b7aaec582edd9c6e70c26ca3
Messbah.A <Messbah.A@Gmail.com>
2024-11-14 14:33:04 +0330

    First Upload

--- src/main/java/com/mftplus/eesample/service/UserService.java
+++ src/main/java/com/mftplus/eesample/service/UserService.java
@@ -14,6 +14,7 @@ public class UserService {
     private EntityManager entityManager;
 
     @Transactional
+    @Loggable
     public void save(User user) {
         entityManager.persist(user);
     }
